3554a2bb8bb4eb62711eb2a215f7c75cb2f4b85a
Mariana Caravanti <mariana.caravanti@gmail.com>
2021-10-18 11:43:06 -0300

    Add files via upload

---
+++ Heterogeneous PU-LP/BoW/results_LPHN.py
@@ -0,0 +1,208 @@
+import sys
+from sklearn.metrics import f1_score
+import pandas as pd
+from os import path
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+import numpy as np
+import urllib.request
+
+def probClasse(f):
+	s = 0
+	p = []
+	for v in f:
+		s += v
+	if s == 0:
+		p = [0,0]
+	else:
+		p = [0,0]
+		for i in range(len(f)):
+			p[i] = f[i] / s
+	return p 
+
+class resultsLPHN():
+	def __init__(self):
+		self.exp_id = sys.argv[1]
+		self.pulp_id = sys.argv[2]
+		self.prop_id = sys.argv[3]
+		self.file_name = sys.argv[4]
+		dir_bow_op = sys.argv[5]
+
+		exp_metadata = pd.read_csv('params.metadata', sep='\t', index_col=0)
+		self.exp_metadata = exp_metadata.loc[self.exp_id]
+
+		pulp_metadata = pd.read_csv(dir_bow_op+'params_pulp.metadata', sep='\t', index_col=0)
+		self.pulp_metadata = pulp_metadata.loc[self.pulp_id]
+
+		prop_metadata =  pd.read_csv(dir_bow_op+'params_labelprop.metadata', sep='\t', index_col=0)
+		self.prop_metadata = prop_metadata.loc[self.prop_id]
+		dir_labels = dir_bow_op.split('/')[:2]
+		dir_labels = '/'.join(dir_labels)
+
+		self.labels = np.load(dir_labels+'/labels.npy')
+		self.indexes = np.load(dir_labels+'/indexes.npy')
+
+		self.folds = self.pulp_metadata.fold.split(',')
+		self.train, self.test = self.calc_train()
+
+		self.calc()
+
+	def calc_train(self):
+		train = []
+		for i in self.folds:
+			with open('folds/fold'+i) as f:
+				for row in f:
+					train.append(row.replace('\n',''))
+
+		test = []
+		with open(self.file_name) as f:
+			for row in f:
+				name, class_vector = row.split('\t')
+				id, layer = name.split(':')
+				if(layer == 'news' and id not in train):
+					test.append(row)
+		return train, test
+
+
+
+
+	def calc(self):
+		
+		self.cmn = self.getCMN()
+		tp = fp = tn = fn = 0
+
+		results = []
+		labels = []
+		
+		for f in self.test:
+			name, class_vector = f.split('\t')
+			name, layer = name.split(':')
+			if(layer=='news'):
+				index_position = np.where(self.indexes == name)
+				label = int(self.labels[index_position[0]])
+			
+				x, y = class_vector.split(',')
+				x, y = float(x), float(y)
+			
+			arg = self.argMaxClass(f=[x,y])
+
+			if(arg == 0 and label == 1):
+				results.append(1)
+				labels.append(1)
+
+			elif(arg== 0 and label == -1):
+				results.append(1)
+				labels.append(-1)
+
+			elif(arg== 1 and label == -1):
+				results.append(-1)
+				labels.append(-1)
+
+			elif(arg==1 and label == 1):
+				results.append(-1)
+				labels.append(1)
+		
+		matriz_confusao = pd.DataFrame(0, columns={'pred_pos', 'pred_neg'},index={'classe_pos', 'classe_neg'})
+		for l in range(len(results)):
+			rotulo_original =  'classe_pos' if labels[l] == 1 else 'classe_neg'
+			predito = 'pred_pos' if results[l] == 1 else 'pred_neg'
+			matriz_confusao[predito][rotulo_original] += 1
+				
+		print(matriz_confusao)
+		
+
+		matriz_confusao.to_csv('results/confusion/confusion_lphn_{}.csv'.format(self.prop_id), sep='\t')
+		positive_total = matriz_confusao['pred_pos'].sum() #total de noticias classificadas como positivo
+		true_positive_news = matriz_confusao['pred_pos'].loc['classe_pos'].sum()
+		 
+		f = open('results/avaliados.csv', 'a')
+
+		id = self.prop_id
+		dataset = self.exp_metadata.dataset
+		representation = self.exp_metadata.representation_model
+		stopwords = self.exp_metadata.stopwords
+		language =  self.exp_metadata.language
+		option = str(self.exp_metadata.option)
+		ngram = str(self.exp_metadata.ngram_range)
+		min_df = str(self.exp_metadata.min_df)
+		norm = self.exp_metadata.norm
+		arg_min_weigth = str(self.exp_metadata.arg_min_weigth)
+
+
+		fold = str(self.pulp_metadata.fold)
+		k = str(self.pulp_metadata.k)
+		a = str(self.pulp_metadata.a)
+		m = str(self.pulp_metadata.m)
+		l = str(self.pulp_metadata.l)
+
+		mi = str(self.prop_metadata.mi)
+		max_iter = str(self.prop_metadata.max_iter)
+		limiar_conv = str(self.prop_metadata.limiar_conv)
+		weight_relations = str(self.prop_metadata.weight_relations)
+
+		list_params_exp = "\t".join([id, dataset, representation, stopwords, language, option, ngram, min_df, norm, arg_min_weigth])
+		list_params_pulp = "\t".join([fold, k, a, m, l])
+		list_params_prop = "\t".join([mi, max_iter, limiar_conv, weight_relations])
+
+		TP = matriz_confusao['pred_pos'].loc['classe_pos'].sum()
+		FP = matriz_confusao['pred_pos'].loc['classe_neg'].sum()
+		TN = matriz_confusao['pred_neg'].loc['classe_neg'].sum()
+		FN = matriz_confusao['pred_neg'].loc['classe_pos'].sum()
+		precision = TP / (TP + FP)
+		recall = TP / (TP + FN)
+		f1 = (2*precision*recall)/(precision+recall)
+
+		f.write("\t".join(['lphn', list_params_exp, list_params_pulp, list_params_prop, 
+			str(f1_score(labels, results, average='macro')), str(f1_score(labels, results, average='micro')), 
+			str(accuracy_score(labels, results)), str(precision_score(labels, results, average='macro')), 
+			str(precision_score(labels, results, average='micro')), str(recall_score(labels, results, average='macro')), 
+			str(recall_score(labels, results, average='micro')), str(true_positive_news/positive_total), 
+			str(precision), str(recall), str(f1)])+'\n')
+
+		f.close()
+		
+		print('Macro f1:', f1_score(labels, results, average='macro'))
+		print('Micro f1:', f1_score(labels, results, average='micro'))
+		print('accuracy:', accuracy_score(labels, results))
+		print('Macro Precision:', precision_score(labels, results, average='macro'))
+		print('Micro Precision:', precision_score(labels, results, average='micro'))
+		print('Macro Recall:', recall_score(labels, results, average='macro'))
+		print('Micro Recall:', recall_score(labels, results, average='micro'))
+		print('True Positive:', true_positive_news/positive_total)
+		print('Interest-class precision: ', precision)
+		print('Interest-class recall:', recall)
+		print('Interest-class f1:', f1)
+
+	def getCMN(self):
+		cmn = [0,0]
+		rodou = False
+		
+		for f in self.test:
+			name, class_vector = f.split('\t')
+			x, y = class_vector.split(',')
+			x, y = float(x), float(y)
+				
+			rodou = True
+			f = [x,y]
+			for i in range(len(f)):
+				cmn[i] += f[i]
+		if rodou:
+			return cmn
+		else:
+			return None
+
+	def argMaxClass(self, f):
+		argMax = -1
+		prob = probClasse(f)
+		maxValue = 2.2250738585072014e-308
+		for i in range(0, 2):
+			value = prob[i] * (f[i] / self.cmn[i])
+			if(value > maxValue):
+				argMax = i
+				maxValue = value
+		return argMax 
+
+
+lphn = resultsLPHN()
---
+++ Heterogeneous PU-LP/D2V/adjacency_matrix.py
@@ -0,0 +1,154 @@
+from util.bibliotecas import *
+from util.functions import *
+
+class MatricesCalc():
+	def __init__(self, exp_id, exp_metadata):
+
+		option = exp_metadata.option
+		input_path = sys.argv[2].format(option,exp_id)
+		input_files = input_path.split('/')[:2]
+		input_files = "/".join(input_files)
+		self.indexes_file = input_files + '/indexes.npy'
+		self.bow_file = input_path+'tf_idf_vect.csv'
+		self.adj_matrix_file = input_path+'representation_input/adj_matrix.csv'
+		self.graphml_output_file = input_path+'representation_input/{}/knn_fakenews.graphml'
+		self.doc_term_relations_output = input_path+'representation_input/{}/doc_term.relations' 	
+		self.output_matriz_w = input_path+'representation_input/{}/{}/w_matrix.csv'
+				
+		arg_k = sys.argv[3].split(',')
+		arg_a = sys.argv[4].split(',')
+		
+		self.arg_k = [int(x) for x in arg_k]
+		self.arg_a = [float(x) for x in arg_a]
+		self.arg_min_weigth = float(sys.argv[5])
+		self.indexes = np.load(self.indexes_file)
+
+		self.tfidf_vect_df = pd.read_csv(self.bow_file, sep=',', index_col=0, header=0)
+
+		self.adjacence_matrix = self.calc_MatrizAdj()
+		self.ident_matrix = self.calc_Indent()
+		self.A = None
+		self.G = None
+		self.calc_knn_w_matrices()		
+
+		
+
+	def calc_Indent(self):
+		print("Calculando matriz identidade...", flush=True)
+		ident_matrix = np.identity(len(self.adjacence_matrix), dtype = float)
+		ident_matrix = pd.DataFrame(ident_matrix, columns=self.indexes, index=self.indexes)
+		return ident_matrix
+
+	
+	def calc_MatrizAdj(self):
+		doc2vec_rep = load_representation("dataset/model.rep")
+
+		adj_matrix = pd.DataFrame(doc2vec_rep.text_vectors, index=self.indexes)
+
+		time_init = time()
+		Y = cdist(adj_matrix, adj_matrix, metric=cosine)
+		adjacence_matrix = pd.DataFrame(Y, columns=self.indexes, index=self.indexes)
+		adjacence_matrix.to_csv(path_or_buf=self.adj_matrix_file)
+		return adjacence_matrix
+
+	
+	def calc_knn_w_matrices(self):
+		print("Calculating k-nn matrices...", flush=True)
+		for valor_k in self.arg_k:
+			print(valor_k)
+
+			print("Calculando {}-NN...".format(valor_k), flush=True)
+			self.A, self.G = self.PU_LP_knn(valor_k)
+
+			self.doc_term_net(valor_k)
+			
+			for alfa in self.arg_a:
+				self.calc_W(valor_k, alfa)
+	
+	def calc_W(self, valor_k, alfa):
+		matrix_w_output_name = self.output_matriz_w.format(valor_k, alfa)
+		
+		print("\tCalculando Índice de Katz aplha={}...".format(alfa), flush=True)
+		#Calcula W = (I - alfa x A)^-1 - I
+		#Medida de centralidade que considera número de vizinhos de um nó e o quão importante esses vizinhos são
+		A = self.A.mul(alfa)
+		I = self.ident_matrix.subtract(A)
+		
+		I = pd.DataFrame(np.linalg.pinv(I.values.astype(np.float32)), columns=self.indexes, index=self.indexes)
+		W = I.subtract(self.ident_matrix)
+
+		W.to_csv(path_or_buf=matrix_w_output_name)
+
+		print('\t\tDesalocar Matriz W', flush=True)
+		del W			
+
+	def PU_LP_knn(self, k):
+		
+		total_columns = self.adjacence_matrix.shape[0]
+		#A é uma matriz com base em k-NN, na qual Aij = 1 se j é um dos k vizinhos de i, e 0 c.c.
+		A = pd.DataFrame(0, columns=self.adjacence_matrix.index, index=self.adjacence_matrix.index)
+		#G é o grafo gerado a partir da matriz A, cujos vértices não estão rotulados
+		G = nx.Graph()
+		for index_i, row in self.adjacence_matrix.iterrows():
+			#knn é um vetor de k posições, inicializadas com valor alto
+			#o vetor knn armazena os k índices de vizinhos mais próximos do vértice i (menor distância de cosseno)
+			knn = [1000 for temp in range(k)]
+			#knn_names armazena os nomes correspondentes aos índices armazenados no vetor knn
+			knn_names = ['' for temp in range(k)]
+			max_value = 1000
+			max_value_id = 0
+			#para cada coluna correspondente a linha i (j vizinhos)
+			for name_j, value in row.iteritems():
+				#se i != j faça
+				if(index_i != name_j):
+					#se a distância é menor que o maior valor armazenado no vetor knn:
+					if (value < max_value):
+						#adiciono vértice j aos vizinhos de i
+						knn_names[max_value_id] = name_j
+						knn[max_value_id] = value
+						max_value_id = np.argmax(knn)
+						max_value = knn[max_value_id]
+			for j in range(k):
+				#Seleciona os 4 vizinhos mais próximos e os adiciono na matriz A
+				vizinho = knn_names[j]
+				A.loc[index_i][vizinho] = 1
+				#Adiciona aresta no grafo entre i e seus 4 vizinhos mais próximos
+				#print(index_i, '\t', vizinho)
+				G.add_edge(index_i, vizinho, weight=1)
+		
+		print("\tSalva informações do Grafo")
+		nx.write_graphml(G, self.graphml_output_file.format(k))
+		return A, G
+
+
+	def doc_term_net(self, valor_k):
+
+		doc_term_relations_output = self.doc_term_relations_output.format(valor_k)
+		fwrite = open(doc_term_relations_output, 'w')
+		
+		for x, y in self.G.edges:
+			node_1 = x + ':news'
+			node_2 = y + ':news'
+			weight = 1
+			edge = node_1 + "\t" + node_2 + "\t" + str(weight) + "\n"
+			fwrite.write(edge)
+
+		for term in self.tfidf_vect_df.columns:
+			for i in range(len(self.indexes)):
+				news = self.indexes[i]
+				weight = self.tfidf_vect_df.loc[news,term]
+				if weight > self.arg_min_weigth:
+					node_1 = news + ':news'
+					node_2 = term + ':term'      
+					edge = node_1 + "\t" + node_2 + "\t" + str(weight) + "\n"
+					fwrite.write(edge)
+
+		fwrite.close()
+
+#Parâmetros de entrada
+exp_id = sys.argv[1]
+exp_metadata = pd.read_csv('params.metadata', sep='\t', index_col=0)
+exp_metadata = exp_metadata.loc[exp_id]
+
+mtc = MatricesCalc(exp_id, exp_metadata)
+
